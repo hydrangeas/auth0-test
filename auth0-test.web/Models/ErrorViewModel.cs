e671c58e15bbe7faae619062040d227ab8d8f8b1
Akira Otaka <hydrangeas.project@gmail.com>
2024-11-17 15:26:50 +0900

    プロジェクト ファイルを追加します。

---
+++ auth0-test.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace auth0_test.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
